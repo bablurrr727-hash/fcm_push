56964197537ab8a41314eae1507cdb99833d6544
bablurrr727-hash <bablurrr727@gmail.com>
2026-05-19 02:43:06 +0530

    Update index.js

--- FCM_SERVER/index.js
+++ FCM_SERVER/index.js
@@ -1,3 +1,20 @@
+const express = require("express");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://rto-1-4b543-default-rtdb.firebaseio.com"
+});
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("SERVER RUNNING");
+});
+
 app.get("/send/:id", async (req, res) => {
 
   try {
@@ -12,7 +29,25 @@ app.get("/send/:id", async (req, res) => {
 
     console.log("SNAPSHOT:", snapshot.val());
 
-    return res.send("STEP 1 OK");
+    const token = snapshot.val();
+
+    if (!token) {
+      return res.send("TOKEN NOT FOUND");
+    }
+
+    const message = {
+      data: {
+        action: "wake"
+      },
+      token: token
+    };
+
+    const response =
+      await admin.messaging().send(message);
+
+    console.log(response);
+
+    return res.send("Push Sent");
 
   } catch (e) {
 
@@ -21,4 +56,8 @@ app.get("/send/:id", async (req, res) => {
     return res.send(e.toString());
   }
 
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("SERVER STARTED");
 });
